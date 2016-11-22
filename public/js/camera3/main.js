6ad701d5be85bcaf228a1e483417a0d774c831d5
sonsory <sonsory@naver.com>
2016-11-22 09:50:08 +0900

    test3-9

--- public/js/camera3/main.js
+++ public/js/camera3/main.js
@@ -123,22 +123,88 @@ function handleError(error) {
 var canvas = document.getElementById('canvas');
 // console.log( "js - canvas : ", canvas )
 var context = canvas.getContext('2d');
-console.log( "context : ", video )
+console.log( "context : ", context )
 
 var video = document.getElementById('video');
 // console.log( "js - video : ", video )
 
 document.getElementById("snap").addEventListener("click", function() {
-	context.drawImage(video, 0, 0, 320, 240);
-  console.log("context:", context)
+console.log("context:", video.videoHeight)
+console.log("context:", video.videoWidth)
+  // $(canvas).width( video.videoWidth /4)
+  // $(canvas).height( video.videoHeight /4)
+  context.drawImage(video, 0, 0, video.videoWidth /4 *1.3  , video.videoHeight /4 *1.3 );
+
 });
 
 document.getElementById("video").addEventListener("click", function() {
-	context.drawImage(video, 0, 0, 210, 150);
+	context.drawImage(video, 0, 0, video.videoWidth /4 *1.3, video.videoHeight /4 *1.3);
   console.log("context:", context)
 });
 
 
+//video와 snap은 scan div로 묶여 있음
+$(document).ready(function(){
+  $("#video").click(function(){
+    $("#scan").hide();
+    $("#view").show();
+  });
+});
+
+$(document).ready(function(){
+  $("#snap").click(function(){
+    $("#scan").hide();
+    $("#view").show();
+  });
+});
+
+
+$(document).ready(function(){
+  $("#canvas").click(function(){
+    $("#view").hide();
+    $("#scan").show();
+  });
+});
+
+
+$(document).ready(function(){
+  $("#backToScan").click(function(){
+    $("#view").hide();
+    $("#scan").show();
+  });
+});
+
+$(document).ready(function(){
+  $("#setButton").click(function(){
+    $("#setButton").hide();
+    $("#scan").hide();
+    $("#set").show();
+    $("#scanButton").show();
+    $("#view").hide();
+
+  });
+});
+
+
+$(document).ready(function(){
+  $("#scanButton").click(function(){
+    $("#scanButton").hide();
+    $("#set").hide();
+    $("#setButton").show();
+    $("#scan").show();
+    $("#view").hide();
+  });
+});
+
+
+
+//시작하면서 숨길 요쇼들 - 1.스캔화면, 2.스캔버튼, 이후 세팅버튼 누르면 스캔버튼 생기고,세팅화면은 스대로 남아있고, 세팅버튼 사라짐, 이후 스캔버튼 누르면, 세팅화면 사라지고 스캔화면으로 변하고, 스캔버튼 사라짐
+window.onload = function(){
+$("#setButton").hide();
+$("#view").hide();
+$("#scan").hide();
+
+};
 
 
 document.getElementById("audioSource").style.display = 'none';
