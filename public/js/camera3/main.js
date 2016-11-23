92465eff1517540cb55ca83979c273a6efdea1d9
Daehoon <kdh812@gmail.com>
2016-11-22 22:31:56 -0800

    Test

--- public/js/camera3/main.js
+++ public/js/camera3/main.js
@@ -133,8 +133,9 @@ console.log("context:", video.videoHeight)
 console.log("context:", video.videoWidth)
   // $(canvas).width( video.videoWidth /4)
   // $(canvas).height( video.videoHeight /4)
-  context.drawImage(video, 0, 480*3.5/16, 640,480, 0, 0, 150, 150);
+  context.drawImage(video, 0, 480*3.5/16+160, 213,160, 0, 0, 150, 150);
 
+  console.log(context.getImageData(0,0,150,150))
 });
 
 document.getElementById("video").addEventListener("click", function() {
